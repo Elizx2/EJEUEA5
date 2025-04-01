13d8cf03e43665eff3cf51c034336feed26cfece
EmilyRosas <2233073411@correo.ler.uam.mx>
2025-04-01 10:53:08 -0600

    avance

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 window.scrollTo(0, document.body.scrollHeight);
 
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".img, .img1");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("v");
+            } else {
+                entry.target.classList.remove("v"); // Se quita para que se repita la animación
+            }
+        });
+    }, { threshold: 1.2 }); // Se activa cuando el 30% del elemento es visible
+
+    images.forEach(img => observer.observe(img));
+});
+
