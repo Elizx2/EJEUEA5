4531b7dacea7735d7965eeaeabd399cf15f57477
EmilyRosas <2233073411@correo.ler.uam.mx>
2025-04-28 22:50:52 -0600

    avance

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ scene.add(ambientLight);
 
 //aquí se insertó el modelo
 const loader = new GLTFLoader();
-loader.load('assets/Mapa3D.glb', (gltf) => {
+loader.load('assets/Mapabueno.glb', (gltf) => {
   console.log('loading model');
   const mesh = gltf.scene;
 
@@ -108,25 +108,25 @@ const estadosInfo = {
    texto: '<strong>Título</strong>: RECOMENDACIÓN No. 47. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS A LA PROTECCIÓN DE LA SALUD Y A LA VIDA POR VIOLENCIA OBSTÉTRICA, EN AGRAVIO DE V1 Y V2.<br><strong> Estado </strong>: Chiapas <br>  <strong>Fecha de emisión:</strong>  30 de septiembre de 2016  <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>' ,
    video: "assets/videos/Caso_1.mp4"
  },
-    "Hospital rural, Zongolica, Veracruz." : {
+    "Hospital rural, Zongolica, Veracruz" : {
    texto: '<strong>Título: </strong> RECOMENDACIÓN NO. 5/2025. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA SALUD MATERNA, A UNA VIDA LIBRE DE VIOLENCIA DE TIPO OBSTÉTRICA Y AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD DE V1 PERSONA INDÍGENA NÁHUATL, ASÍ COMO AL PROYECTO DE VIDA DE V1, QVI1, VI5 Y VI6, Y A LA INTEGRIDAD DE VI2, VI3 Y VI4 EN EL HOSPITAL RURAL ZONGOLICA DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN ZONGOLICA, VERACRUZ.<br><strong> Estado </strong>: Veracruz <br>  <strong>Fecha de emisión:</strong>   31 de enero de 2025 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2025-03/REC_2025_005.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
    video: "assets/videos/Caso_2.mp4"
  },
    
-    " Ciudad Madero, Tamaulipas.": {
+    "Ciudad Madero, Tamaulipas": {
      texto: '<strong>Título: </strong>RECOMENDACIÓN NO. 212 /2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD Y A UNA VIDA LIBRE DE VIOLENCIA EN SU MODALIDAD OBSTÉTRICA EN AGRAVIO DE V1; A LA SALUD, A LA VIDA Y AL PRINCIPIO DEL INTERÉS SUPERIOR DE LA NIÑEZ EN AGRAVIO DE V2; ASÍ COMO AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO Y AFECTACIÓN AL PROYECTO DE VIDA DE QVI, V1, VI1 Y VI2, POR PERSONAL MÉDICO EN LA UNIDAD DE MEDICINA FAMILIAR NO. 77 Y EN EL HOSPITAL GENERAL REGIONAL NO. 6, AMBOS DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL, EN CIUDAD MADERO, TAMAULIPAS.<br><strong> Estado </strong>: Tamaulipas <br>  <strong>Fecha de emisión:</strong>   23 septiembre de 2024 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-09/REC_2024_212.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_4.mp4"
    },
-    " Reynosa, Tamaulipas.": {
+    "Reynosa, Tamaulipas": {
      texto: '<strong>Título: </strong>RECOMENDACIÓN NO. 204/2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A UNA VIDA LIBRE DE VIOLENCIA OBSTÉTRICA, A LA PROTECCIÓN DE LA SALUD, A LA VIDA Y AL TRATO DIGNO EN AGRAVIO DE V; ASÍ COMO, AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO DE QVI Y VI, ATRIBUIBLE AL PERSONAL MÉDICO DEL HOSPITAL GENERAL REGIONAL NO. 270 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN REYNOSA, TAMAULIPAS.<br><strong> Estado </strong>: Tamaulipas <br>  <strong>Fecha de emisión:</strong>    30 de agosto de 2024 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-09/REC_2024_204.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_5.mp4"
    },
-    "Matamoros, Tamaulipas." : {
+    "Matamoros, Tamaulipas" : {
     texto: '<strong>Título: </strong>RECOMENDACIÓN NO. 260 /2024 SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD Y A UNA VIDA LIBRE DE VIOLENCIA EN SU MODALIDAD GINECO-OBSTÉTRICA EN AGRAVIO DE QV; ASÍ COMO, AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO DE QV, VI1 Y VI2, ATRIBUIBLE AL PERSONAL MÉDICO DEL HOSPITAL GENERAL DE ZONA NO. 13 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN MATAMOROS, TAMAULIPAS.<br><strong> Estado </strong>: Tamaulipas<br>  <strong>Fecha de emisión:</strong>    20 de noviembre de 2024<strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-11/REC_2024_260.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
     video: "assets/videos/Caso_10.mp4"
   },
 
-    " Villa de Álvarez, Colima.": {
+    "Villa de Álvarez, Colima": {
      texto: '<strong>Título: </strong>RECOMENDACIÓN No. 280/2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD; ASÍ COMO A UNA VIDA LIBRE DE VIOLENCIA EN SU MODALIDAD GINECO OBSTÉTRICA EN AGRAVIO DE V1; Y AL INTERÉS SUPERIOR DE LA NIÑEZ Y A LA PROTECCIÓN A LA VIDA DE V2; ASÍ COMO, AL DERECHO DE ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO DE V1, QVI Y VI1, POR PERSONAL MÉDICO EN EL HOSPITAL GENERAL DE ZONA NO. 1 “VILLA DE ÁLVAREZ", DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL, EN EL MUNICIPIO VILLA DE ÁLVAREZ, COLIMA.<br><strong> Estado </strong>: Colima <br>  <strong>Fecha de emisión:</strong>    18 de diciembre  de 2024 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2025-01/REC_2024_280.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_6.mp4"
    },
@@ -135,25 +135,25 @@ const estadosInfo = {
    video: "assets/videos/Caso_7.mp4"
  },
 
-    "Hospital General, Torreón, Coahuila." : {
+    "Hospital General, Torreón, Coahuila" : {
      texto: '<strong>Título: </strong>RECOMENDACIÓN NO. 19/2025. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA SALUD MATERNA, A UNA VIDA LIBRE DE VIOLENCIA DE TIPO OBSTÉTRICA, AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO DE V1, ASÍ COMO AL PROYECTO DE VIDA DE V1, VI1 Y VI2, ATRIBUIBLES A PERSONAL DEL HOSPITAL RURAL NÚMERO 79 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN MATAMOROS, COAHUILA Y DEL HOSPITAL GENERAL DE LA SECRETARÍA DE SALUD DEL ESTADO DE COAHUILA, EN TORREÓN, COAHUILA. <br><strong> Estado </strong>: Coahuila <br>  <strong>Fecha de emisión:</strong>    28 de febrero de 2025 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2025-03/REC_2025_019.pdf" target="_blank">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_8.mp4"
    },
 
-    " Ciudad Juárez, Chihuahua." : {
+    "Ciudad Juárez, Chihuahua" : {
    texto: '<strong>Título: </strong> RECOMENDACIÓN NO. 177 /2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD MATERNA, A UNA VIDA LIBRE DE VIOLENCIA OBSTÉTRICA, Y AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD DE QV1; AL INTERÉS SUPERIOR DE LA NIÑEZ Y A LA PROTECCIÓN DE LA SALUD DE V2, ASÍ COMO AL PROYECTO DE VIDA QV1 Y V2 EN HOSPITAL GENERAL REGIONAL NO. 66 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN CIUDAD JUÁREZ, CHIHUAHUA..<br><strong> Estado </strong>: Chihuahua <br>  <strong>Fecha de emisión:</strong>   17 de julio de 2024 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-08/REC_2024_177.pdf">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
    video: "assets/videos/Caso_3.mp4"
  },
-    " San Francisco del Oro, Chihuahua." : {
+    "San Francisco del Oro, Chihuahua" : {
      texto: '<strong>Título: </strong> RECOMENDACIÓN NO. 18/2025. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA SALUD MATERNA, A UNA VIDA LIBRE DE VIOLENCIA OBSTÉTRICA, EN AGRAVIO DE QV1, ASÍ COMO DAÑO AL PROYECTO DE VIDA DE QV1, VI1, VI2 Y VI3 EN LA UNIDAD DE MEDICINA FAMILIAR NO. 09 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL, EN SAN FRANCISCO DEL ORO, CHIHUAHUA.<br><strong> Estado </strong>: Chihuahua <br>  <strong>Fecha de emisión:</strong>   28 de febrero de 2025 <strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2025-03/REC_2025_018_0.pdf">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_9.mp4"
    },
    
-    " Ciudad de México" : {
+    "Ciudad de México" : {
      texto: '<strong>Título: </strong> RECOMENDACIÓN NO. 178/2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD MATERNA, A UNA VIDA LIBRE DE VIOLENCIA EN SU MODALIDAD DE VIOLENCIA OBSTÉTRICA Y AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD DE QV1; ASÍ COMO AL PROYECTO DE VIDA DE QV1, VI1 Y VI2 EN EL HOSPITAL DE GINECO OBSTETRICIA NO. 4 “LUIS CASTELAZO AYALA”, DE LA UNIDAD MÉDICA DE ALTA ESPECIALIDAD DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL EN LA CIUDAD DE MÉXICO.<br><strong> Estado </strong>: Ciudad de México<br>  <strong>Fecha de emisión:</strong>   17 de julio de 2024<strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-08/REC_2024_178.pdf">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_11.mp4"
    },
-    " Hospital General, Torreón, Coahuila.": {
+    " Hospital General, Torreón, Coahuila": {
      texto: '<strong>Título: </strong> RECOMENDACIÓN No. 241/ 2024. SOBRE EL CASO DE VIOLACIONES A LOS DERECHOS HUMANOS A LA PROTECCIÓN DE LA SALUD, AL TRATO DIGNO, ASÍ COMO UNA VIDA LIBRE DE VIOLENCIA EN SU MODALIDAD OBSTÉTRICA EN AGRAVIO DE V; ASÍ COMO, AL ACCESO A LA INFORMACIÓN EN MATERIA DE SALUD EN AGRAVIO DE QV, VI1 Y VI2, POR PERSONAL MÉDICO EN LOS HOSPITALES GENERALES DE ZONA NÚMERO 6 Y 16 DEL INSTITUTO MEXICANO DEL SEGURO SOCIAL, AMBOS EN COAHUILA.<br><strong> Estado </strong>: Coahuila<br>  <strong>Fecha de emisión:</strong>   31 de octubre de 2024<strong> <br>*Basado en documentación de la Comisión Nacional de los Derechos Humanos (CNDH).</strong> <br> Documento de referencia: <br>  <a href="https://www.cndh.org.mx/sites/default/files/documentos/2024-11/REC_2024_241.pdf">https://www.cndh.org.mx/sites/default/files/doc/Recomendaciones/2016/Rec_2016_047.pdf</a>',
      video: "assets/videos/Caso_12.mp4"
    }
@@ -271,48 +271,48 @@ function mostrarInfoEstado(nombre, info) {
 function detectarEstadoPorCoordenadas(point) {
 const tolerance = 0.3;
 
-if (Math.abs(point.x -  -0.7468047095862027) < tolerance && Math.abs(point.z -  0.6531382854475947) < tolerance) {
+if (Math.abs(point.x - -0.818237832461091) < tolerance && Math.abs(point.z -  -0.389685680719726  ) < tolerance) {
   return "Ciudad de México"; // Coordenadas para CDMX
 }
 
-if (Math.abs(point.x - -2.709238450240913) < tolerance && Math.abs(point.z - 0.5134484823386423 ) < tolerance) {
+if (Math.abs(point.x - -3.041259414261113) < tolerance && Math.abs(point.z - -0.5099626147688241  ) < tolerance) {
   console.log("Estado detectado: Villa de Álvarez, Colima");
   return "Villa de Álvarez, Colima"; // Coordenadas para Colima
 }
 
-if (Math.abs(point.x - 2.6995877952668215) < tolerance && Math.abs(point.z - 1.7485323456303599) < tolerance) {
+if (Math.abs(point.x -  2.964144719712966) < tolerance && Math.abs(point.z - 1.128274342980296) < tolerance) {
   console.log("Estado detectado: Ocosingo, Chiapas");
   return "Ocosingo, Chiapas"; 
 }
 
 if (Math.abs(point.x - 0.6697062116845324) < tolerance && Math.abs(point.z - 1.3582407595541253) < tolerance) {
   console.log("Estado detectado:Hospital rural, Zongolica, Veracruz.");
-  return "Hospital rural, Zongolica, Veracruz."; 
+  return "Hospital rural, Zongolica, Veracruz"; 
 }
 
-if (Math.abs(point.x -  -3.2677151742500024) < tolerance && Math.abs(point.z - -2.350290436165001) < tolerance) {
-  console.log("Estado detectado: San Francisco del Oro, Chihuahua.");
-  return " San Francisco del Oro, Chihuahua."; 
+if (Math.abs(point.x -  -3.3891595084433357) < tolerance && Math.abs(point.z - -2.3107904031675215) < tolerance) {
+  console.log("Estado detectado: San Francisco del Oro, Chihuahua");
+  return "San Francisco del Oro, Chihuahua"; 
 }
 
-if (Math.abs(point.x - -3.545792382729405) < tolerance && Math.abs(point.z - -4.314791041035321) < tolerance) {
-  console.log("Ciudad Juárez, Chihuahua.");
-  return "Ciudad Juárez, Chihuahua."; 
+if (Math.abs(point.x - -2.0464915040041007) < tolerance && Math.abs(point.z - 1.4895657844397743) < tolerance) {
+  console.log("Ciudad Juárez, Chihuahua");
+  return "Ciudad Juárez, Chihuahua"; 
 }
 
 
 if (Math.abs(point.x - -0.27424782001276193) < tolerance && Math.abs(point.z - -1.887456566978174) < tolerance) {
-  console.log("Estado detectado:Ciudad Madero, Tamaulipas.");
-  return "Ciudad Madero, Tamaulipas."; 
+  console.log("Estado detectado:Ciudad Madero, Tamaulipas");
+  return "Ciudad Madero, Tamaulipas"; 
 }
 
 if (Math.abs(point.x - -0.7857313542936664) < tolerance && Math.abs(point.z - -2.455557021329403 ) < tolerance) {
-  console.log("Estado detectado: Reynosa, Tamaulipas.");
-  return " Reynosa, Tamaulipas."; 
+  console.log("Estado detectado: Reynosa, Tamaulipas");
+  return " Reynosa, Tamaulipas"; 
 }
 
 if (Math.abs(point.x -  -0.05457702903959893) < tolerance && Math.abs(point.z - -1.7856534189543727 ) < tolerance) {
-  console.log("Estado detectado: Matamoros, Tamaulipas.");
+  console.log("Estado detectado: Matamoros, Tamaulipas");
   return "Matamoros, Tamaulipas."; 
 }
 
@@ -321,14 +321,14 @@ if (Math.abs(point.x - 0.24109094542950826) < tolerance && Math.abs(point.z - 1.
   return "Hospital rural No.12, Zongolica, Veracruz"; 
 }
 
-if (Math.abs(point.x -  -1.9096383680677569) < tolerance && Math.abs(point.z - -2.882090843195982 ) < tolerance) {
-  console.log("Estado detectado:Hospitales Generales de Zona NO. 6 y 16, Torreón, Coahuila.");
-  return "Hospitales Generales de Zona NO. 6 y 16, Torreón, Coahuila."; 
+if (Math.abs(point.x -  -1.7551767983785342) < tolerance && Math.abs(point.z - -2.0335167200004047) < tolerance) {
+  console.log("Estado detectado:Hospitales Generales de Zona NO. 6 y 16, Torreón, Coahuila");
+  return "Hospitales Generales de Zona NO. 6 y 16, Torreón, Coahuila"; 
 }
 
-if (Math.abs(point.x - -2.20869465417342) < tolerance && Math.abs(point.z - -1.6131558294082895) < tolerance) {
-  console.log(" Hospital General, Torreón, Coahuila.");
-  return " Hospital General, Torreón, Coahuila."; 
+if (Math.abs(point.x - -2.244481563060835) < tolerance && Math.abs(point.z - -1.5787312166864003) < tolerance) {
+  console.log("Hospital General, Torreón, Coahuila");
+  return "Hospital General, Torreón, Coahuila"; 
 }
 return null;
 }
